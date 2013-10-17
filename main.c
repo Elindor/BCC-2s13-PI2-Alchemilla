e058cbbd77f12d535aabc498cf60775a1a82f207
Elindor <ganopper@gmail.com>
2013-10-17 17:35:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -15,16 +15,19 @@
 
 
 
-int out1, out2, outb;					//Variaveis globais, os numeros pra saida da tela de elementos.
-int fase;						//Variavel global, a fase do jogo.
-char checklist[11], startlist[11], itemlist[11];	//Nomes dos arquivos que serão usados
+int out1, out2, outb;                   //Variaveis globais, os numeros pra saida da tela de elementos.
+int fase;                       //Variavel global, a fase do jogo.
+char checklist[12], startlist[12], itemlist[12];    //Nomes dos arquivos que serão usados
+int target;
+char targetname[30];
+int tabreagentes[2][10];
 
 
 
 struct element{
     int ElNum;
     char ElName[30];
-    int Eltype;
+    int ElType;
     struct element *prox;;
 };
 typedef struct element lista;
@@ -33,15 +36,15 @@ typedef struct element lista;
 
 
 void filenamesgen(){
-	sprintf (checklist, "check%d.txt", fase);		//
-	sprintf (startlist, "start%d.txt", fase);		// Isto gera os nomes dos 3 arquivos necessários para as leituras de cada função.
-	sprintf (itemlist, "stage%d.txt", fase);		//
+    sprintf (checklist, "check%02d.txt", fase);    //
+    sprintf (startlist, "start%02d.txt", fase);    // Isto gera os nomes dos 3 arquivos necessários para as leituras de cada função.
+    sprintf (itemlist, "stage%02d.txt", fase);    //
 }
 
 
-                                        //*********************************************//
-void insert(int num, lista menu){       // LEMBRAR: O nome da lista TEM QUE SER MENU!!!//
-    lista *p, *q;                       //*********************************************//
+
+void insert(int num, lista *menu){
+    lista *p, *q;
     p = menu;
     q = menu -> prox;
     while(q != NULL){
@@ -52,86 +55,86 @@ void insert(int num, lista menu){       // LEMBRAR: O nome da lista TEM QUE SER 
     }
     lista *nova;
     nova = malloc(sizeof (lista));
-
+    
     FILE *file;
     int x;
-    char confere;
     file = fopen(itemlist, "r");
-    do
-        fscanf(file, "%c", &confere);       // procura traços / saidas.
-        if(confere == '-'){
+    do{
+        fscanf(file, "%d", &x);       // procura traços / saidas.
+        if(x == -1){
             fscanf(file, "%d", &x);         //verifica primeiro numero -> checa com valor num.
             if(x == num){
                 nova -> ElNum = num;
-                fscanf(file, "%d", &nova -> ElName);
+                while(confere != '\0'){
+                    fscanf(file, "%s", nova -> ElName);
+                    
+                }
                 fscanf(file, "%d", &nova -> ElType);
-                /*
-                ou
-                fscanf(file, "%d", &x);
-                nova -> ElName = x;
-                fscanf(file, "%d", &x);
-                nova -> ElType = x;
-                */
+                
                 nova -> prox = NULL;
                 fclose(file);
                 return;
             }
         }
-    while(confere != '!');
+    }while(x != -2);
     fclose(file);
     return ;
 }
 
-//*****************************************************************************************
-//***********************                INCOMPLETO               *************************
-//*****************************************************************************************
-lista start_menu(){
-    FILE *start;
-    char confere;
+
+void start_menu(lista *menu){
+    FILE *file;
     int x;
-    file = fopen(startlist, "r");
-    do
-        fscanf(file, "%c", &confere);       // Vai até o começo da checagem.
-    while(confere != ':');
-
-    do
+    file = fopen("start00.txt", "r");
+    
     fscanf(file, "%d", &x);
-    insert(x, menu);
-    while(x != 45);
-
-
+    while(x != -1){
+        insert(x, menu);
+        fscanf(file, "%d", &x);
+    }    
+    fscanf(file, "%d", &target);    //pega numero do objetivo p/ global
+    fscanf(file, "%s", targetname); //pega nome do objetivo p/ global
+    
+    fscanf(file, "%d", &x); // Só pra tirar o próximo -1
+    
+    fscanf(file, "%d", &x);
+    while(x != -1){
+        printf("registra %d\n\n", x);
+        fscanf(file, "%d", &x);
+    }
+    printf("EXIT");
+    
+    fclose(file);
+    
+    return ;
 }
-//*****************************************************************************************
+
 
 
 
 int checagem(int in1, int in2, int reag){
-	FILE *file;
-	int aux;					//Usado em ordenação e conferir entradas
-	char confere;               			// Usado nos do/while e conferir reagente
+    FILE *file;
+    int aux;                    //Usado em ordenação e conferir entradas
+    char confere;                           // Usado nos do/while e conferir reagente
     
     
-	if(in1 > in2){
-		aux = in1;
-		in1 = in2;
-		in2 = aux;
-	}
+    if(in1 > in2){
+        aux = in1;
+        in1 = in2;
+        in2 = aux;
+    }
     
-	file = fopen(checklist, "r");
+    file = fopen(checklist, "r");
     
-	do
-        fscanf(file, "%c", &confere);		// Vai até o começo da checagem.
-	while(confere != ':');
-    
-	do{
-		fscanf(file, "%d", &aux);
-		if(aux == in1){
+    do{
+        fscanf(file, "%d", &aux);
+        if(aux == in1){
             fscanf(file, "%d", &aux);
-			if(aux == in2){
-				fscanf(file, "%d", &aux);
-				if(aux == reag){
+            if(aux == in2){
+                fscanf(file, "%d", &aux);
+                if(aux == reag){
                     
-					fscanf(file, "%d", &aux);
+                    fscanf(file, "%d", &aux);
                     if(aux != 0){
                         out1 = aux;
                     }
@@ -146,20 +149,26 @@ int checagem(int in1, int in2, int reag){
                         outb = aux;
                         
                     }
-		  			
-                    fclose(file);
-                    return 1;
                     
-				}
-			}
-		}
+                    fclose(file);
+                    return 1;               // Se retornar um, faz checagem na lista INFO pelo nome dos elementos.
+                    
+                }
+            }
+        }
         
-		do{
-			fscanf(file, "%c", &confere);
-		}while(confere != '-' || confere != '!');
-		
-	}while(confere != '!');
+        do{
+            fscanf(file, "%d", &x);
+        }while(x < 0);
+        
+    }while(x != -2);
     fclose(file);
     return 0;
-} 
- 
+}
+
+lista inicializa_lista(){
+    
+    lista *l = malloc(sizeof(lista));
+    l -> prox = NULL;
+    return *l;
+}
