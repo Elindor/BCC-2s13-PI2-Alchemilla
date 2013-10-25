93dc710b073cae43b8266a466dd6452bfa8f3b9c
Elindor <ganopper@gmail.com>
2013-10-25 18:05:24 -0200

    Update leitura.h

--- Codigo/leitura.h
+++ Codigo/leitura.h
@@ -8,9 +8,10 @@ void start_menu(lista *menu);
 char * nomeia(int num);
 int checagem(int in1, int in2, int reag);
 lista inicializa_lista();
+void nomeia_reag(int reagente, int i);
 
-extern int out1, out2, fase, target, tabreagentes[2][10]; 
-extern char out1name, out2name, checklist[12], startlist[12], itemlist[12], infolist[11], targetname[30];
+extern int out1, out2, fase, target, reagentes[10]; 
+extern char out1name, out2name, checklist[12], startlist[12], itemlist[12], infolist[11], targetname[30], reagname[30][10];
 
 
 #endif
