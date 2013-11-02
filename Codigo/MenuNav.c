d9a623f8ed1f8db1f9a7271e1b28edb15d73f4f3
DaniloIkuta <danilo0195@hotmail.com>
2013-11-02 15:51:50 -0200

    Agora usando al_draw_text

--- Codigo/MenuNav.c
+++ Codigo/MenuNav.c
@@ -1,5 +1,8 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
 #include <stdio.h>
 //#include <stdbool.h>
 
@@ -20,13 +23,14 @@ ALLEGRO_BITMAP *botao3 = NULL;
 ALLEGRO_BITMAP *botao4 = NULL;
 ALLEGRO_BITMAP *mainMenu = NULL;
 ALLEGRO_BITMAP *selectMenu = NULL;
+ALLEGRO_FONT *fonte = NULL;
 
 //Protótipos
 void fadeInOut(ALLEGRO_BITMAP *img, int velocidade, int restTime);                  //Função de fade in, espera e fade out
 int intro();                                                                       //Chamada simplificada de fadeInOut
 bool checkSair(ALLEGRO_EVENT *evento);                                              //Verifica se o ícone de fechar programa foi acionado
-bool checkBotao(ALLEGRO_BITMAP *botao, float x, float y, ALLEGRO_EVENT *evento);    //Verifica se o mouse está sobre o botão
-bool clickBotao(ALLEGRO_BITMAP *botao, float x, float y, ALLEGRO_EVENT *evento);    //Verifica se o botão foi clicado
+bool checkBotao(float xa, float xb, float ya, float yb, ALLEGRO_EVENT *evento);    //Verifica se o mouse está sobre o botão
+bool clickBotao(float xa, float xb, float ya, float yb, ALLEGRO_EVENT *evento);    //Verifica se o botão foi clicado
 bool init();                                                                        //Inicia os componentes
 bool introInit();
 void finish();                                                                      //Limpa os componentes
@@ -51,49 +55,49 @@ int main(){
         al_clear_to_color(al_map_rgb(0, 0, 0));                                 //Limpa a tela
         al_draw_bitmap(mainMenu, 0, 0, 0);
 
-        if(clickBotao(botao1, 824.5, 126, &evento)){
+        if(clickBotao(740, 909, 98, 154, &evento)){
             //Ir p/MenuB
         }
 
-        else if(clickBotao(botao2, 825, 215, &evento)){
+        else if(clickBotao(741, 909, 187, 243, &evento)){
             intro();
         }
 
-        else if(clickBotao(botao3, 825, 305, &evento)){
+        else if(clickBotao(741, 909, 277, 333, &evento)){
             intro();
             finish();
             return 0;
         }
 
-        else if(clickBotao(botao4, 824.5, 393, &evento)){
+        else if(clickBotao(740, 909, 365, 421, &evento)){
             finish();
             return 0;
         }
 
         //Checagens de maouse sobre botão
-        if(checkBotao(botao1, 824.5, 126, &evento))        //Caso o mouse esteja em cima do botão
-            al_draw_tinted_bitmap(botao1, al_map_rgba(128, 128, 128, 128), 774.5, 76, 0);   //Draw com mask
+        if(checkBotao(740, 909, 98, 154, &evento))        //Caso o mouse esteja em cima do botão
+            al_draw_text(fonte, (al_map_rgb(128, 0, 0)), 824.5, 98, ALLEGRO_ALIGN_CENTRE, "Jogar");
 
         else
-            al_draw_bitmap(botao1, 774.5, 76, 0);         //Draw padrão
+            al_draw_text(fonte, (al_map_rgb(0, 0, 0)), 824.5, 98, ALLEGRO_ALIGN_CENTRE, "Jogar");
 
-        if(checkBotao(botao2, 825, 215, &evento))        //Caso o mouse esteja em cima do botão
-            al_draw_tinted_bitmap(botao2, al_map_rgba(128, 128, 128, 128), 775, 165, 0);   //Draw com mask
-
-        else
-            al_draw_bitmap(botao2, 775, 165, 0);         //Draw padrão
-
-        if(checkBotao(botao3,825, 305, &evento))        //Caso o mouse esteja em cima do botão
-            al_draw_tinted_bitmap(botao3, al_map_rgba(128, 128, 128, 128), 775, 255, 0);   //Draw com mask
+        if(checkBotao(741, 909, 187, 243, &evento))        //Caso o mouse esteja em cima do botão
+            al_draw_text(fonte, (al_map_rgb(128, 0, 0)), 825, 187, ALLEGRO_ALIGN_CENTRE, "Intro");
 
         else
-            al_draw_bitmap(botao3, 775, 255, 0);         //Draw padrão
+            al_draw_text(fonte, (al_map_rgb(0, 0, 0)), 825, 187, ALLEGRO_ALIGN_CENTRE, "Intro");
 
-        if(checkBotao(botao4, 824.5, 393, &evento))        //Caso o mouse esteja em cima do botão
-            al_draw_tinted_bitmap(botao4, al_map_rgba(128, 128, 128, 128), 774.5, 343, 0);   //Draw com mask
+        if(checkBotao(741, 909, 277, 333, &evento))        //Caso o mouse esteja em cima do botão
+           al_draw_text(fonte, (al_map_rgb(128, 0, 0)), 825, 277, ALLEGRO_ALIGN_CENTRE, "Intro e sair");
+
+        else  
+            al_draw_text(fonte, (al_map_rgb(0, 0, 0)), 825, 277, ALLEGRO_ALIGN_CENTRE, "Intro e sair");
+
+        if(checkBotao(740, 909, 365, 421, &evento))        //Caso o mouse esteja em cima do botão
+            al_draw_text(fonte, (al_map_rgb(128, 0, 0)), 824.5, 365, ALLEGRO_ALIGN_CENTRE, "Sair");
 
         else
-            al_draw_bitmap(botao4, 774.5, 343, 0);         //Draw padrão
+            al_draw_text(fonte, (al_map_rgb(0, 0, 0)), 824.5, 365, ALLEGRO_ALIGN_CENTRE, "Sair");
 
         al_flip_display();                                                      //Atualiza a tela
     }
@@ -111,6 +115,14 @@ bool init(){                                                                    
         fprintf(stderr, "Allegro image nao foi carregado.\n");
         return false;
     }
+
+    al_init_font_addon();
+
+    if(!al_init_ttf_addon()){
+        fprintf(stderr, "Erro ao carregar plugin de ttf\n");
+        return false;
+    }
+
 
     janela = al_create_display(larguraTela, alturaTela);                            //Cria a janela
     if(!janela){                                                                    //Se a janela não for criada
@@ -136,6 +148,12 @@ bool init(){                                                                    
     if(!filaEventos){                                                               //Fila não criada
         fprintf(stderr, "Fila de eventos nao foi criada.\n");
         al_destroy_display(janela);
+        return false;
+    }
+
+    fonte = al_load_font("Fonte/anirom.ttf", 22, 0);
+    if(!fonte){
+        fprintf(stderr, "Erro ao carregar fonte\n");
         return false;
     }
 
@@ -193,6 +211,7 @@ void finish(){                          //Desaloca a memória
     al_destroy_bitmap(selectMenu);
     al_destroy_display(janela);
     al_destroy_event_queue(filaEventos);
+    al_destroy_font(fonte);
 }
 
 int intro(){                       //Chamada simplificada de fadeInOut
@@ -239,33 +258,31 @@ bool checkSair(ALLEGRO_EVENT *evento){                                      //Ve
     return false;
 }
 
-bool checkBotao(ALLEGRO_BITMAP *botao, float x, float y, ALLEGRO_EVENT *evento){    //Verifica se o mouse está sobre o botão
+bool checkBotao(float xa, float xb, float ya, float yb, ALLEGRO_EVENT *evento){    //Verifica se o mouse está sobre o botão
     al_register_event_source(filaEventos, al_get_mouse_event_source());                 //Registra fonte dos eventos (mouse)
 
     al_wait_for_event_timed(filaEventos, evento, 0.001);                                //Espera 0.001 até que algum evento apareça
 
-    if(evento->mouse.x >= x - al_get_bitmap_width(botao)/2 &&                           //Calcula a "hitbox" do bitmap
-    evento->mouse.x <= x + al_get_bitmap_width(botao)/2 &&                              //Draw bitmap tem como referencia o pixel esquerdo superior,
-    evento->mouse.y >= y - al_get_bitmap_height(botao)/2 &&                             //mas para comparar área, usa-se o pixel central
-    evento->mouse.y <= y + al_get_bitmap_height(botao)/2){
+    if(evento->mouse.x >= xa &&                                                         //Calcula a "hitbox" do bitmap
+    evento->mouse.x <= xb &&                                                            //Draw bitmap tem como referencia o pixel esquerdo superior,
+    evento->mouse.y >= ya &&                                                            //mas para comparar área, usa-se o pixel central
+    evento->mouse.y <= yb)
         return true;
-    }
 
     return false;                                                       //Caso o mouse não esteja nessa "hitbox"
 }
 
-bool clickBotao(ALLEGRO_BITMAP *botao, float x, float y, ALLEGRO_EVENT *evento){    //Verifica se o botão foi clicado
+bool clickBotao(float xa, float xb, float ya, float yb, ALLEGRO_EVENT *evento){    //Verifica se o botão foi clicado
     al_register_event_source(filaEventos, al_get_mouse_event_source());                 //Registra fonte dos eventos (mouse)
     
     al_wait_for_event_timed(filaEventos, evento, 0.001);                                //Espera 0.001 até que algum evento apareça
 
     if(evento->type == ALLEGRO_EVENT_MOUSE_BUTTON_UP && evento->mouse.button == LMB){   //Além de calcular a hitbox, verifica o evento foi um clique
-        if(evento->mouse.x >= x - al_get_bitmap_width(botao)/2 &&                       //E se foi com o botão esquerdo do mouse
-        evento->mouse.x <= x + al_get_bitmap_width(botao)/2 &&
-        evento->mouse.y >= y - al_get_bitmap_height(botao)/2 &&
-        evento->mouse.y <= y + al_get_bitmap_height(botao)/2){
+        if(evento->mouse.x >= xa &&                                                     //E se foi com o botão esquerdo do mouse
+        evento->mouse.x <= xb &&
+        evento->mouse.y >= ya &&
+        evento->mouse.y <= yb)
             return true;
-        }
     }
 
     return false;
